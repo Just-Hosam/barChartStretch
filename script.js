85523989d2f5f353860fb1fda617841744762145
Hosam  Dahrooge <Hosam_firas@hotmail.com>
2021-01-20 20:41:05 -0700

    added the type of data that will be used in the bar chart (object)

---
+++ script.js
@@ -0,0 +1,36 @@
+const data = [
+  {
+    name: 'Housing',
+    rent: 500,
+    utilities: 100
+  },
+
+  {
+    name: 'Subscriptions',
+    ytPremium: 12,
+    spotify: 10,
+    audible: 15
+  },
+
+  {
+    name: 'Food',
+    groceries: 350,
+    skipTheDishes: 150
+  }
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// drawBarChart(data, options, element);
