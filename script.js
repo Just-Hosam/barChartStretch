199cdb731985cd4cb0bca285e0283a33651f5f03
Hosam  Dahrooge <Hosam_firas@hotmail.com>
2021-01-23 00:14:33 -0700

    Chose data type (object). Wrote JQuery to make "bar" elements in html Wrote JQuery to add css height to each bar based on given data

--- script.js
+++ script.js
@@ -1,21 +1,59 @@
-const data = [
+const dataTest = [
   {
-    name: 'Housing',
-    rent: 500,
+    name: 'January',
+    numData: 15
   },
 
   {
-    name: 'Subscriptions',
-    ytPremium: 12,
+    name: 'February',
+    numData: 25
   },
 
   {
-    name: 'Food',
-    groceries: 350,
+    name: 'March',
+    numData: 65
+  },
+
+  {
+    name: 'April',
+    numData: 75
+  },
+
+  {
+    name: 'May',
+    numData: 45
   }
 ]
 
+let newDataTest = [
+  ['January', '15%'],
+  ['February', '25%'],
+  ['March', '65%'],
+  ['April', '75%']
+]
 
+$(document).ready(() => {
+  for (let j = 0; j < dataTest.length; j++) {
+    let dataPercent = dataTest[j].numData;
+    let dataName = dataTest[j].name;
+    let newStr = "<li><div class='bar' data-percentage='" + dataPercent + "'></div><span>" + dataName + "</span></li>";
+    $('.bars').append(newStr);
+  }
+
+  $('.bars li .bar').each((i, value) => {
+    value.dataset.percentage = dataTest[i].numData;
+    let heightPercent = value.dataset.percentage + '%';
+    $(value).css('height', heightPercent);
+  });
+
+
+});
+
+/*    let divElement = $('<div></div>').text('');
+    $('.bars li').append(divElement);
+    let spanElement = $('<span></span>').text('');
+    $('.bars li div').append(spanElement);
+    */
 
 
 
