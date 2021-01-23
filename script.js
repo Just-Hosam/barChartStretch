d0f95132266f14ace9b8ed61e90d58abd16b0544
Hosam  Dahrooge <Hosam_firas@hotmail.com>
2021-01-23 10:04:45 -0700

    Adjusted the css connection to show the data values on bars, not percentage.

--- script.js
+++ script.js
@@ -1,59 +1,67 @@
-const dataTest = [
+const expenseDataObj = [
   {
     name: 'January',
-    numData: 15
+    numData: 600,
+    numUnit: '$'
   },
 
   {
     name: 'February',
-    numData: 25
+    numData: 350,
+    numUnit: '$'
   },
 
   {
     name: 'March',
-    numData: 65
+    numData: 400,
+    numUnit: '$'
   },
 
   {
     name: 'April',
-    numData: 75
+    numData: 135,
+    numUnit: '$'
   },
 
   {
     name: 'May',
-    numData: 45
-  }
+    numData: 500,
+    numUnit: '$'
+  },
+
 ]
 
-let newDataTest = [
-  ['January', '15%'],
-  ['February', '25%'],
-  ['March', '65%'],
-  ['April', '75%']
-]
+const largestValue = dataObj => {
+  let storeArr = [];
+  for(let i = 0; i < expenseDataObj.length; i++) {
+    storeArr.push(dataObj[i].numData);
+  }
+
+  return Math.max(...storeArr);
+}
+
 
 $(document).ready(() => {
-  for (let j = 0; j < dataTest.length; j++) {
-    let dataPercent = dataTest[j].numData;
-    let dataName = dataTest[j].name;
-    let newStr = "<li><div class='bar' data-percentage='" + dataPercent + "'></div><span>" + dataName + "</span></li>";
+  for (let j = 0; j < expenseDataObj.length; j++) {
+    let fromDataSet = expenseDataObj[j].numData;
+    let dataPercent = Math.floor( 100 * fromDataSet / largestValue(expenseDataObj) );
+    let dataName = expenseDataObj[j].name;
+
+    let newStr = "<li><div class='bar' data-percentage='" + dataPercent +  "' value='" + fromDataSet + expenseDataObj[j].numUnit + "'></div><span>" + dataName + "</span></li>";
+
     $('.bars').append(newStr);
   }
 
   $('.bars li .bar').each((i, value) => {
-    value.dataset.percentage = dataTest[i].numData;
-    let heightPercent = value.dataset.percentage + '%';
+    value.dataset.percentage = 100 * expenseDataObj[i].numData / largestValue(expenseDataObj);
+
+    let heightPercent = Math.floor(value.dataset.percentage) + '%';
+
     $(value).css('height', heightPercent);
   });
 
 
 });
-
-/*    let divElement = $('<div></div>').text('');
-    $('.bars li').append(divElement);
-    let spanElement = $('<span></span>').text('');
-    $('.bars li div').append(spanElement);
-    */
 
 
 
