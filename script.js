7e0975115fd4c97110134229d3295329d7f4ff82
Hosam  Dahrooge <Hosam_firas@hotmail.com>
2021-01-24 20:16:21 -0700

    Added dynamic features for the chart title commented and removed any console.logs

--- script.js
+++ script.js
@@ -1,16 +1,4 @@
-options = {
-  unit: '$',
-  barColor: 'red',
-  labelColor: 'black',
-  titleText: 'this is not right',
-  titleColor: 'black',
-  titleFontSize: 25,
-  titleFontUnit: 'px',
-  yAxisHeightValue: 300,
-  yAxisHeightUnit: 'px'
-}
-
-
+// helper function to find the largest value in an object
 const largestValue = dataObj => {
   let storeArr = [];
   for(let i = 0; i < dataObj.length; i++) {
@@ -20,6 +8,7 @@ const largestValue = dataObj => {
   return Math.max(...storeArr);
 }
 
+// main function that draws the chart
 const drawBarChart = (expenseDataObj, options) => {
 
   $(document).ready(() => {
@@ -66,20 +55,17 @@ const drawBarChart = (expenseDataObj, options) => {
       }
     })
 
-    // updates the bar charts to what the user wants
+    // updates the bars to what the user wants
     $('.bars .bar').css('background-color', options.barColor);
     $('.bars li').css('color', options.labelColor);
-    $('#barTitle').html(options.titleText);
-    $('#barTitle').css('color', options.titleColor);
-    let titleFontSizeCont = options.titleFontSize + options.titleFontUnit
-    $('#barTitle').css('font-size', titleFontSizeCont);
+
 
     // adjusts bar, y-axis & y-axis values heightes to match given height
-    let yAxisHeight = options.yAxisHeightValue + options.yAxisHeightUnit;
+    let yAxisHeight = options.yAxisHeightValue + 'px';
     $('.bars').css('height', yAxisHeight);
     $('.bars li').css('height', yAxisHeight);
     $('.y-axis-values').css('height', yAxisHeight);
-    let spaceBtnTicks = options.yAxisHeightValue/2 + options.yAxisHeightUnit;
+    let spaceBtnTicks = options.yAxisHeightValue/2 + 'px';
     $('.y-axis-values li').css('height', spaceBtnTicks);
 
 
@@ -101,17 +87,35 @@ $(document).ready(() => {
     tableRow += '<td><input type="button" value="Delete Row" class="deleteButton"></td>';
     tableRow += '</tr>';
     $('table tbody').append(tableRow);
-  })
+  });
 
   // deletes table row on button click
   $('table tbody').on('click',  '.deleteButton',(event) => {
     $(event.currentTarget).closest('tr').remove();
-  })
+  });
 
   // deletes ALL table rows on button click
   $('#resetButton').on('click',() => {
     $('tbody tr').remove();
-  })
+  });
+
+
+  // dynamically sets the chart title
+  $('#title-text').on('keyup', () => {
+    let newStr = $('#title-text')[0].value;
+    $('#barTitle').html(newStr);
+  });
+
+  // dynamically sets the color of the chart title
+  $('#title-color').on('change', () => {
+    $('#barTitle').css('color', $('#title-color')[0].value);
+  });
+
+  // dynamically sets the font size of the chart title
+  $('#title-size').on('change', () => {
+    let titleSizeAndUnit = $('#title-size')[0].value + 'px';
+    $('#barTitle').css('font-size', titleSizeAndUnit);
+  });
 
 
 
@@ -120,13 +124,17 @@ $(document).ready(() => {
 // data collection
 
 let dataArr = [];
+let options = {};
 
+// function that on submission resets the chart, takes input from the user and draws the bars for the chart
 const getTableData = () => {
   $(document).ready(() => {
     $('.submissionBtn').on('click', () => {
       dataArr = [];
       barChartReset();
       let counter = 0;
+
+      // loops through input table and sets data into the dataArr
       $('table tbody tr').each((index, key) => {
         dataArr.push({})
         dataArr[index].name = $('table tbody tr input')[counter].value;
@@ -135,12 +143,14 @@ const getTableData = () => {
         counter += 2;
       })
 
+      // updates the customization options for the bar chart
       options.unit = $('#cust-labelUnit')[0].value;
       options.barColor = $('#cust-barColor')[0].value;
       options.labelColor = $('#cust-labelColor')[0].value;
       options.yAxisHeightValue = $('#cust-yAxisHeightValue')[0].value;
-      options.yAxisHeightUnit = $('#cust-yAxisUnit')[0].value;
 
+
+      // prevents undefined values incase of an empty submission
       if(dataArr.length > 0) {
         drawBarChart(dataArr, options);
       } else {
@@ -150,6 +160,7 @@ const getTableData = () => {
   })
 }
 
+// function to reset the values and bars on the chart bar
 const barChartReset = () => {
   $('#topTick').empty();
   $('#midTick').empty();
